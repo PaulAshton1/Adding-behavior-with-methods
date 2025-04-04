a76c9c99ab67a17862dd6c29de3c5e6b6ce3852d
PaulAshton1 <ashtonvilla42@gmail.com>
2025-04-04 11:45:40 +0300

    updated file

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Your code here
+
+class Cat{
+    constructor(name, sex){
+        this.name = name;
+        this.age = sex;
+    }
+
+    meow(){
+        return `${this.name} says meow!`;
+    }
+} 
+
+class Dog{ 
+    constructor(name,sex) {
+        this.name = name;
+        this.age = sex;
+    }
+     
+    bark(){
+       return `${this.name}says woof!`;
+    }
+
+}
+
+class Bird {
+    constructor(name, sex) {
+        this.name = name;
+        this.sex = sex;
+    }
+
+    speak() {
+        if (this.sex === 'male') {
+            return `It's me! ${this.name}, the parrot!`;
+        } else {
+            return `${this.name} says squawk!`;
+        }
+    }
+}
+
+let myCat = new Cat('Will','female');
+console.log(myCat.speak()); // Will says meow!
+
+let myDog = new Dog('Bruno', 'male');
+console.log(myDog.speak());// Bruno says woof!
+
+let myBird = new Bird ('Holly','male');
+console.log(myBird.speak()); // It's me! Holly, the parrot!
+
+let myOtherBird = new Bird('Cate', 'female');
+console.log(myOtherBird.speak()); // Cate says squawk!
+
